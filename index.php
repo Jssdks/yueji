9d062a6ef6ce57b5f79e5d6cde26c71accc8b759
aibin <lab366@163.com>
2024-10-16 19:49:44 +0800

    db

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
 
-echo "欢迎来到YUE.已丽人生活馆";
+echo "欢迎来到YUE.已丽人生活馆\n";
 try {
     $pdo = new PDO('mysql:host=mysql;dbname=mysql', 'root', '123456');
     // 设置 PDO 错误模式为异常
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "连接成功"; 
+    echo "DB连接成功\n"; 
 } catch(PDOException $e) {
-    die("连接失败: " . $e->getMessage());
+    die("DB连接失败: " . $e->getMessage()."\n");
 }
+
+// 执行查询
+    $stmt = $pdo->query('SELECT * FROM some_user');
+    while ($row = $stmt->fetch()) {
+        // 处理结果
+        print_r($row);
+    }
 
 ?>
