7ef27a8f8fdadf104767512eeb9e2b79bbb7007b
aibin <lab366@163.com>
2024-10-17 20:33:25 +0800

    redis

--- index.php
+++ index.php
@@ -6,27 +6,36 @@ try {
 	// 设置 PDO 错误模式为异常
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	echo "DB连接成功\n";
-	// 开始执行查询
-	$stmt = $pdo->query('SELECT * FROM dict_city');
-	while ($row = $stmt->fetch()) {
-		// 处理结果
-		print_r($row);
-		break;
-	}
 } catch (PDOException $e) {
 	die("DB连接失败: " . $e->getMessage() . "\n");
 }
 
+// 开始执行查询
+$stmt = $pdo->query('SELECT * FROM dict_city');
+while ($row = $stmt->fetch()) {
+	// 处理结果
+	print_r($row);
+	break;
+}
 
+$redis = new Redis();
 try {
-	$redis = new Redis();
 	$redis->connect('redis', 6379);
 	echo "REDIS连接成功\n";
-	$redis->set("params",time());
-	echo $redis->get("params");
 } catch (RedisException $e) {
 	die("REDIS连接失败: " . $e->getMessage() . "\n");
 }
 
+try {
+	$redis->set("params", time());
+} catch (RedisException $e) {
+	die("REDIS写失败: " . $e->getMessage() . "\n");
+}
+try {
+	echo $redis->get("params");
+} catch (RedisException $e) {
+	die("REDIS读失败: " . $e->getMessage() . "\n");
 
-?>
+}
+
+
