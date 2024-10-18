1db6edaa15cbd701323e7b8cfc15a4ff919dc5e1
aibin <lab366@163.com>
2024-10-18 09:00:59 +0800

    redis

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
-echo "欢迎来到YUE.已丽人生活馆\n";
+echo "欢迎来到YUE.已丽人生活馆\n<br />";
 try {
 	$pdo = new PDO('mysql:host=mysql;dbname=yueji_test', 'root', '123456');
 	// 设置 PDO 错误模式为异常
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "DB连接成功\n";
+	echo "DB连接成功\n<br />";
 } catch (PDOException $e) {
-	die("DB连接失败: " . $e->getMessage() . "\n");
+	die("DB连接失败: " . $e->getMessage() . "\n<br />");
 }
 
 // 开始执行查询
@@ -21,20 +21,21 @@ while ($row = $stmt->fetch()) {
 $redis = new Redis();
 try {
 	$redis->connect('redis', 6379);
-	echo "REDIS连接成功\n";
+	$redis->auth("k7z9x*t[j=M^5){e");
+	echo "REDIS连接成功\n<br />";
 } catch (RedisException $e) {
-	die("REDIS连接失败: " . $e->getMessage() . "\n");
+	die("REDIS连接失败: " . $e->getMessage() . "\n<br />");
 }
 
 try {
 	$redis->set("params", time());
 } catch (RedisException $e) {
-	die("REDIS写失败: " . $e->getMessage() . "\n");
+	die("REDIS写失败: " . $e->getMessage() . "\n<br />");
 }
 try {
 	echo $redis->get("params");
 } catch (RedisException $e) {
-	die("REDIS读失败: " . $e->getMessage() . "\n");
+	die("REDIS读失败: " . $e->getMessage() . "\n<br />");
 
 }
 
